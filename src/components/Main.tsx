638d6c306d92b53c6b492a82ce28818db30e0fea
Matheus Bickel <devbickel@gmail.com>
2022-03-19 14:30:24 -0300

    changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { CardCenter, MainCard } from './styled'
+import { CardCenter, FinalCard, MainCard, FinalCard2 } from './styled'
 
 function Main() {
     return (
         <MainCard>
             <CardCenter>
-                
+                <FinalCard></FinalCard>
+                <FinalCard2></FinalCard2>
+
             </CardCenter>
         </MainCard>
     );
--- src/components/styled.js
+++ src/components/styled.js
@@ -2,10 +2,32 @@ import styled from 'styled-components'
 
 export const MainCard = styled.div`
     height: 100vh;
-    background-color: black;
+    background-image: url("https://tse4.mm.bing.net/th?id=OIP.5qWFyLIfzQl4NNkx-cdqSgHaEK&amp");
 `;
 
 export const CardCenter =  styled.div`
     height: 50vh;
+    position: absolute;
+    top: 0; bottom: 0;
+    left: 0; right: 0;
+    margin-left: 200px;
+    margin-right: 200px;
+    margin-top: 200px;
+`;
+
+export const FinalCard = styled.div`
+    height: 220px;
+    width: 220px;
     background-color: red;
+    margin-left: 70px;
+    margin-right: 70px;
+`;
+
+export const FinalCard2 = styled.div`
+    height: 220px;
+    width: 220px;
+    background-color: red;
+    margin-left: 70px;
+    margin-right: 70px;
+    float: right;
 `;
