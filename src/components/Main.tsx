a336748e9f939f537b80773952907c9adba1ba8e
Matheus Bickel <devbickel@gmail.com>
2022-03-19 13:56:48 -0300

    primeiros componentes

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CardCenter, MainCard } from './styled'
+
+function Main() {
+    return (
+        <MainCard>
+            <CardCenter>
+                
+            </CardCenter>
+        </MainCard>
+    );
+}
+
+export default Main;
---
+++ src/components/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const MainCard = styled.div`
+    height: 100vh;
+    background-color: black;
+`;
+
+export const CardCenter =  styled.div`
+    height: 50vh;
+    background-color: red;
+`;
